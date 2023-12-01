e588d0487d7e76ec26f608de6607d4d18d3ef754
Tran Ba Thanh <77083684+thanhtb-2411@users.noreply.github.com>
2023-12-01 15:44:18 +0700

    feat: Create nestjs project structure (#19)

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './api/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
