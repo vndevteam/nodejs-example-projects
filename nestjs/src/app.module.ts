d73f970ca4026a2c12a254c8e475708559f30cfa
Tran Ba Thanh <77083684+thanhtb-2411@users.noreply.github.com>
2023-12-01 16:29:00 +0700

    feat: config environment (#25)

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './api/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
