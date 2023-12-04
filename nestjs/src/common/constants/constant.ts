dad6ac8dd072fab03a8e3acc70034fb4c57ad318
bathanh <tran.ba.thanh@sun-asterisk.com>
2023-12-04 09:11:47 +0700

    feat: config interceptor

--- nestjs/src/common/constants/constant.ts
+++ nestjs/src/common/constants/constant.ts
@@ -1,6 +1,12 @@
 export const ENV_KEYS = {
+  NODE_ENV: 'nodeEnv',
   DATABASE: 'db',
   JWT_SECRET: 'jwt.secret',
   JWT_EXPIRE_TIME: 'jwt.expiresIn',
   JWT_REFRESH_SECRET: 'jwt.refreshSecret',
+};
+
+export const NODE_ENV = {
+  DEVELOPMENT: 'development',
+  PRODUCTION: ' production',
 };
