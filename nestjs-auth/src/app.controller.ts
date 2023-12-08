6be14953a8588e9fd70fb5130b627c2257185ee8
Lam Ngoc Khuong <lam.ngoc.khuong@sun-asterisk.com>
2023-12-08 09:57:01 +0700

    chore: rename folder

--- nestjs-auth/src/app.controller.ts
+++ nestjs-auth/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Public } from './auth-typeorm/decorators/public.decorator';
+import { Public } from './auth-typeorm/decorator/public.decorator';
 
 @Controller()
 export class AppController {
--- nestjs-auth/src/auth-typeorm/auth.controller.ts
+++ nestjs-auth/src/auth-typeorm/auth.controller.ts
@@ -10,7 +10,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './guards/local-auth.guard';
+import { LocalAuthGuard } from './guard/local-auth.guard';
 
 @Controller('')
 export class AuthController {
