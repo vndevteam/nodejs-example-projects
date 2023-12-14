250368f2827ac5c384a27c836a33c43311381db3
Lam Ngoc Khuong <49505176+khuongln-1346@users.noreply.github.com>
2023-12-14 10:17:48 +0700

    feat(i18n): add i18n function (#37)

--- nestjs/src/api/users/users.controller.ts
+++ nestjs/src/api/users/users.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { I18n, I18nContext } from 'nestjs-i18n';
 
 @Controller('users')
 export class UsersController {
   private logger = new Logger(UsersController.name);
   constructor(private readonly usersService: UsersService) {}
   @Get()
-  findAll() {
-    return 'This action returns all users';
+  findAll(@I18n() i18n: I18nContext) {
+    return i18n.t('admin.users.findAll');
   }
 
   @Post()
